cb04620934fb755b41cbfa30df7e838079e16568
cheeseburgertony <1821955020@qq.com>
2024-12-30 21:18:57 +0800

    feat: 使用md5为密码加密

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -5,12 +5,10 @@ class UserController {
     // 1.获取用户传递过来的信息
     const user = ctx.request.body;
 
-    
-
-    // 3.将用户信息存储到数据库中
+    // 2.将用户信息存储到数据库中
     const result = await userService.create(user);
 
-    // 4.查看存储的结果吗，告知前端创建成功
+    // 3.查看存储的结果吗，告知前端创建成功
     ctx.body = {
       message: "创建用户成功~",
       data: result,
--- src/middleware/user.middleware.js
+++ src/middleware/user.middleware.js
@@ -3,7 +3,9 @@ const {
   NAME_ALREADY_EXISTS,
 } = require("../config/error");
 const userService = require("../service/user.service");
+const md5password = require("../utils/md5-password");
 
+// 验证用户传递过来的数据是否符合规范
 const verifyUser = async (ctx, next) => {
   // 2.验证客户端传递过来的user是否可以保存到数据库中
   // 2.1.验证用户名和密码不能为空
@@ -22,6 +24,18 @@ const verifyUser = async (ctx, next) => {
   await next();
 };
 
+// 对密码进行加密
+const handlePassword = async (ctx, next) => {
+  // 1.取出密码
+  const { password } = ctx.request.body;
+
+  // 2.对密码进行加密
+  ctx.request.body.password = md5password(password);
+
+  await next();
+};
+
 module.exports = {
   verifyUser,
+  handlePassword,
 };
