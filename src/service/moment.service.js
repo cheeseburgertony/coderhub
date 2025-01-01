b3ed8cf58fa5535f858dede7200fd43ac81bc189
cheeseburgertony <1821955020@qq.com>
2025-01-01 16:32:42 +0800

    feat: 在动态详情中新增标签列表

--- src/service/moment.service.js
+++ src/service/moment.service.js
@@ -12,7 +12,8 @@ class MomentService {
       SELECT
         m.id id, m.content content, m.createAt createTime, m.updateAt updateTime,
         JSON_OBJECT('id',u.id, 'name', u.name, 'creatTime', u.creatAt, 'updateTime', u.updateAt) user,
-        (SELECT COUNT(*) FROM comment WHERE comment.moment_id = m.id) commentCount
+        (SELECT COUNT(*) FROM comment WHERE comment.moment_id = m.id) commentCount,
+        (SELECT COUNT(*) FROM moment_label ml WHERE ml.moment_id = m.id) labelCount
       FROM moment m
       LEFT JOIN user u
       ON m.user_id = u.id
@@ -32,16 +33,25 @@ class MomentService {
         m.id id, m.content content, m.createAt createTime, m.updateAt updateTime,
         JSON_OBJECT('id',u.id, 'name', u.name, 'creatTime', u.creatAt, 'updateTime', u.updateAt) user,
         (
+          SELECT
           JSON_ARRAYAGG(JSON_OBJECT(
             'id', c.id, 'content', c.content, 'comment_id', c.comment_id,
             'user', JSON_OBJECT('id', cu.id, 'name', cu.name)
           ))
-        ) comments
+          FROM comment c
+          LEFT JOIN user cu ON c.user_id = cu.id
+          WHERE m.id = c.moment_id
+        ) comments,
+        (
+          JSON_ARRAYAGG(JSON_OBJECT(
+            'id', l.id, 'name', l.name
+          ))
+        ) labels
       FROM moment m
       LEFT JOIN user u ON m.user_id = u.id
-      LEFT JOIN comment c ON m.id = c.moment_id
-      LEFT JOIN user cu ON c.user_id = cu.id
-      WHERE m.id = 1
+      LEFT JOIN moment_label ml ON m.id = ml.moment_id
+      LEFT JOIN label l ON ml.label_id = l.id
+      WHERE m.id = ?
       GROUP BY m.id;
     `;
 
